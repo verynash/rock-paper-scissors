4d22999f2c10a135a49883dabeb6319d3ff2a997
verynash <sbz.chary@gmail.com>
2022-07-25 13:12:34 -0400

    Add logic for endGame and playAgain

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const buttons = document.querySelectorAll('button');
+const rockButton = document.getElementById('rock');
+const paperButton = document.getElementById('paper');
+const scissorsButton = document.getElementById('scissors');
+
+rockButton.addEventListener('click', () => handleClick('rock'));
+paperButton.addEventListener('click', () => handleClick('paper'));
+scissorsButton.addEventListener('click', () => handleClick('scissors'));
+
+
 function getComputerChoice() {
     let rpsNumber = Math.floor(Math.random()*3) + 1;
     let computerChoice;
@@ -11,34 +21,17 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-const buttons = document.querySelectorAll('button');
-const rockButton = document.getElementById('rock');
-const paperButton = document.getElementById('paper');
-const scissorsButton = document.getElementById('scissors');
-
-rockButton.addEventListener('click', () => handleClick('rock'));
-paperButton.addEventListener('click', () => handleClick('paper'));
-scissorsButton.addEventListener('click', () => handleClick('scissors'));
-
-
 function handleClick(playerChoice) {
     playRound(playerChoice);
     console.log(playerScore, computerScore)
 }
 
-/*function getPlayerChoice() {
-    let playerChoice;
-    do {
-        playerChoice = prompt("Rock, paper, or scissors?").toLowerCase();
-    }
-    while (playerChoice !== "rock" && playerChoice !== "paper" && playerChoice !== "scissors");
-    return playerChoice;
-}*/
-
 let playerScore = 0;
 let computerScore = 0;
 let playerWins = false;
 let tie = false;
+let winnerText = document.getElementById('winner-text');
+let scoreText = document.getElementById('score-text');
 
 function playRound(playerChoice) {
     let computerChoice = getComputerChoice();
@@ -60,45 +53,55 @@ function playRound(playerChoice) {
         playerWins = false;
     }
 
+    showScore();
+    endGame();
+    playAgain();
+
     if (tie == true) {
+        winnerText.textContent = `Tie! You both picked ${playerChoice}!`;
         console.log(`Tie! You both picked ${playerChoice}!`);
         return tie;
     } else if (playerWins == true) {
+        winnerText.textContent = `You win this round! ${playerChoice} beats ${computerChoice}!`
         console.log(`You win! ${playerChoice} beats ${computerChoice}!`);
         return playerWins;
     } else {
+        winnerText.textContent = `You lose this round! ${computerChoice} beats ${playerChoice}!`
         console.log(`You Lose! ${computerChoice} beats ${playerChoice}!`);
         return playerWins;
     }
 }
 
-/* function playGame() {
-    for (let i = 0; i < 5; i++) {
-        playRound();
-        if (playerWins == true) {
-            playerScore++;
-        } else if (tie == false && playerWins == false) {
-            computerScore++;
-        } else continue;
+function showScore() {
+    scoreText.textContent = `The current score is -- You: ${playerScore}, Alien: ${computerScore}`;
+}
+
+let endGameText = document.getElementById('endGameText');
+let endGameModal = document.getElementById('endGameModal');
+let game = document.querySelector('.game-container');
+
+function endGame() {
+    if (playerScore === 5) {
+        endGameText.textContent = 'CONGRATULATIONS! YOU WIN!'
+        endGameModal.style.display = 'block';
+        game.style.display = 'none';
     }
 
-    if (playerScore > computerScore) {
-        console.log('Congratulations, player wins!');
-    } else if (computerScore > playerScore) {
-        console.log('Better luck next time! Computer wins!');
-    } else {
-        console.log('WOW! You tied!')
+    else if (computerScore === 5) {
+        endGameText.textContent = 'YOU HAVE FAILED THE GALAXY'
+        endGameModal.style.display = 'block';
+        game.style.display = 'none';
     }
-    playAgain();
-} */
+    else return;
+}
+
+let playAgainButton = document.getElementById('playAgain');
 
 function playAgain() {
-    reset = prompt("Play again? y/n").toLowerCase();
-    if (reset == 'y') {
+    playAgainButton.addEventListener('click', () => {
         playerScore = 0;
         computerScore = 0;
-        playGame();
-    } else {
-        console.log('Thanks for playing!')
-    }
+        endGameModal.style.display = 'none';
+        game.style.display = 'block';
+    })
 }
